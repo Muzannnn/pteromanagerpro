fdb8db761b6349ac08fae15e14757971b163c3f9
Muzan <46395979+Muzannnn@users.noreply.github.com>
2025-01-16 22:38:04 +0100

    Update add-service.php

--- php_views/user_cp/add-service.php
+++ php_views/user_cp/add-service.php
@@ -43,39 +43,98 @@
             </div>
         </nav>
         <!-- End Navbar -->
-        <div class="container-fluid py-4 h-100">
+        <div class="container-fluid py-4 h-100 p-3">
+
             <div class="row mt-lg-4 mt-2">
-                <ul class="nav nav-pills nav-fill p-1" role="tablist">
-                    
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center active" id="home-tab"
-                            data-bs-toggle="tab" data-bs-target="#home-tab-pane" href="#" role="tab" aria-controls="home-tab-pane" aria-selected="true">
-                            <i class="ni ni-app"></i>
-                            <span class="ms-2">App</span>
-                        </a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center" id="profile-tab"
-                            data-bs-toggle="tab" data-bs-target="#profile-tab-pane" href="#" role="tab" aria-controls="profile-tab-pane" aria-selected="true">
-                            <i class="ni ni-app"></i>
-                            <span class="ms-2">App</span>
-                        </a>
-                    </li>
 
-                </ul>
-                <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab"
-                        tabindex="0">...</div>
-                    <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab"
-                        tabindex="0">...</div>
-                    <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab"
-                        tabindex="0">...</div>
+
+                <div class="col-lg-2 nav flex-column " id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <button class="nav-link btn-dark btn text-white active" id="v-pills-minecraft-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-minecraft" type="button" role="tab" aria-controls="v-pills-minecraft"
+                        aria-selected="true">Minecraft</button>
+                    <button class="nav-link btn-dark btn text-white" id="v-pills-profile-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile"
+                        aria-selected="false">Profile</button>
+                    <button class="nav-link btn-dark btn text-white" id="v-pills-messages-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages"
+                        aria-selected="false">Messages</button>
+                    <button class="nav-link btn-dark btn text-white" id="v-pills-settings-tab" data-bs-toggle="pill"
+                        data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings"
+                        aria-selected="false">Settings</button>
                 </div>
+                <div class="col-lg-10 tab-content" id="v-pills-tabContent">
+                    <div class="tab-pane fade show active" id="v-pills-minecraft" role="tabpanel"
+                        aria-labelledby="v-pills-minecraft-tab" tabindex="0">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-6 mb-lg-4 mb-4">
+                                <div class="card">
+                                    <div class="card-header text-center pt-4 pb-2">
+                                        <img class="mb-3" src="https://cdn2.steamgriddb.com/icon/374d37146f8e631a2adb03fd6818b497.png" width="90em" alt="mc"><br>
+                                        <span class="text-uppercase font-weight-bold text-gray">Basic</span>
+                                        <h1 class="font-weight-bold mt-2">
+                                            5<small>€</small>/Mois
+                                        </h1>
+                                    </div>
+                                    <div class="card-body text-lg-start text-center pt-0">
+                                        <div class="d-flex justify-content-lg-start justify-content-center p-2">
+                                            <div
+                                                class="text-center">
+                                                <i class="fas fa-users opacity-10"></i>
+                                            </div>
+                                            <div>
+                                                <span class="ps-3">50 slots</span>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex justify-content-lg-start justify-content-center p-2">
+                                            <div
+                                                class="text-center">
+                                                <i class="fas fa-microchip opacity-10"></i>
+                                            </div>
+                                            <div>
+                                                <span class="ps-3">1 vCPU / Intel Xeon 8Ghz</span>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex justify-content-lg-start justify-content-center p-2">
+                                            <div
+                                                class="text-center">
+                                                <i class="fas fa-memory opacity-10"></i>
+                                            </div>
+                                            <div>
+                                                <span class="ps-3">500mb de RAM</span>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex justify-content-lg-start justify-content-center p-2">
+                                            <div
+                                                class="text-center">
+                                                <i class="fas fa-hard-drive opacity-10"></i>
+                                            </div>
+                                            <div>
+                                                <span class="ps-3">15Go d'espace disque</span>
+                                            </div>
+                                        </div>
+                                        <a href="javascript:;" class="btn btn-icon btn-dark d-lg-block mt-3 mb-0">
+                                            Commander
+                                            <i class="fas fa-arrow-right ms-1"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel"
+                        aria-labelledby="v-pills-profile-tab" tabindex="0">
+                    </div>
+                    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel"
+                        aria-labelledby="v-pills-messages-tab" tabindex="0">...</div>
+                    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel"
+                        aria-labelledby="v-pills-settings-tab" tabindex="0">...</div>
+                </div>
+
+
+                <?php include("php_views/components/footer.php") ?>
+
             </div>
-
-
-            <?php include("php_views/components/footer.php") ?>
-
         </div>
     </main>
 
